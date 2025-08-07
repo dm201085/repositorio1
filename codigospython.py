8fd7b16603d7616de3b7bcfec78cc2508bf665e5
dm201085 <debora.dantas10@aluno.ce.gov.br>
2025-08-07 09:12:27 -0300

    quetao 2 feita

--- codigospython.py
+++ codigospython.py
@@ -1,7 +1,10 @@
 #listanúmeros = [1,2,3,4,5,6,7,8,9,10]
 #for n in  list
 
-n1 = 11
-while n1 >= 1:
-    n1 -= 1 
-    print(n1)
+número = 11
+while número >= 1:
+    número -= 1
+    print (número)
+
+
+
