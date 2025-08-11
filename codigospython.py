d568285b9f3beeedab65514f98f0760ef05ea92d
dm201085 <debora.dantas10@aluno.ce.gov.br>
2025-08-11 12:00:49 -0300

    feito ate a questao 6

--- codigospython.py
+++ codigospython.py
@@ -1,10 +1,52 @@
 #listanúmeros = [1,2,3,4,5,6,7,8,9,10]
 #for n in  list
 
-número = 11
-while número >= 1:
-    número -= 1
-    print (número)
+#número = 11
+#while número >= 1:
+#    número -= 1
+#    print (número)
+
+
+#pedirnomes = 0
+#while pedirnomes <= 5:
+    #nome = input ("digite um nome")
+    #print (f"o nome digitdo foi {nome}")
+    #pedirnomes += 1
+    #if pedirnomes == 5:
+     #print("você digitou 5 nomes,vamos parar por aqui")
+     #break
+#print('oi)
+
+#for n in range (21):
+    #if n % 2 ==0:
+       #print(n)
+
+
+#senha = 1234
+#n = int(input("digite uma senha"))
+#while n == 1234 :
+    #print("acertou")
+    #break
+
+#numero = 0
+#while numero <= 4:
+#   int(input('digite um numero'))
+#   numero += 1
+   
+#soma += 
+
+
+
+soma = 0
+numero = 0
+while numero <= 4:
+    n1 = int(input("digite um numero"))
+    numero += 1
+    soma += n1
+    print(f"{soma}")
+    
+
+
 
 
 
