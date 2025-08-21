2d189da3c8b04889f06cd1f5a2e2fc1c4afe8c05
dm201085 <debora.dantas10@aluno.ce.gov.br>
2025-08-21 09:09:32 -0300

    feito questao 2

--- codigo2.py
+++ codigo2.py
@@ -1,9 +1,23 @@
 # 
 
 
+# n1 = int(input("digite uma nota"))
+# n2 = int(input("digite mais uma nota"))
+# n3 = int(input("digite outra nota"))
+
+# média = ((n1+n2+n3)/3)
+
+# if média >= 7:
+#     print("aprovado")
+# else:
+#     print("reprovado")
 
 
 
-n1 = input("digite uma nota")
-n2 = input("digite mais uma nota")
-n3 = input('digite mais uma notaa')
+totalvogais = 0
+ palavra = input(Digite uma palavra)
+vogais =[a,e,i,o,u,A,E,I,O,U]
+for carecte in palavra:
+    if carecte in vogais:
+        totalvogais +=1
+print(f"a{palavra} tem {totalvogais} vogais")
